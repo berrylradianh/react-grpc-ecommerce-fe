4eb11b95b27e0f1f09a5e938d0f2154d59e4fc6d
berrylradianh <berrylhamesha@gmail.com>
2025-06-02 00:20:07 +0700

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,91 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AdminCustomerListSection from './components/AdminCustomerListSection/AdminCustomerListSection';
+import AdminDashboardSection from './components/AdminDashboardSection/AdminDashboardSection';
+import AdminOrderListSection from './components/AdminOrderListSection/AdminOrderListSection';
+import AdminProductListSection from './components/AdminProductListSection/AdminProductListSection';
+import AdminSalesReportSection from './components/AdminReportSection/AdminReportSection';
+import ChangePasswordSection from './components/ChangePasswordSection/ChangePasswordSection';
+import OrderDetailSection from './components/OrderDetailSection/OrderDetailSection';
+import OrderHistorySection from './components/OrderHistorySection/OrderHistorySection';
+import AdminLayout from './layouts/AdminLayout/AdminLayout';
+import AuthLayout from './layouts/AuthLayout/AuthLayout';
+import ECommerceLayout from './layouts/ECommerceLayout/ECommerceLayout';
+import AdminCreateProduct from './pages/AdminCreateProduct/AdminCreateProduct';
+import AdminDashboard from './pages/AdminDashboard/AdminDashboard';
+import AdminOrderDetail from './pages/AdminOrderDetail/AdminOrderDetail';
+import AdminProfile from './pages/AdminProfile/AdminProfile';
+import Cart from './pages/Cart/Cart';
+import Checkout from './pages/Checkout/Checkout';
+import CheckoutSuccess from './pages/CheckoutSuccess/CheckoutSuccess';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import NotFound from './pages/NotFound/NotFound';
+import Profile from './pages/Profile/Profile';
+import Register from './pages/Register/Register';
+import Services from './pages/Services/Services';
+import Shop from './pages/Shop/Shop';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <ECommerceLayout />,
+        children: [
+            { index: true, element: <Home /> },
+            { path: 'shop', element: <Shop /> },
+            { path: 'services', element: <Services /> },
+            { path: 'cart', element: <Cart /> },
+            { path: 'checkout', element: <Checkout /> },
+            { path: 'checkout/success', element: <CheckoutSuccess /> },
+            {
+                path: 'profile',
+                element: <Profile />,
+                children: [
+                    { path: 'change-password', element: <ChangePasswordSection /> },
+                    { path: 'orders', element: <OrderHistorySection /> },
+                    { path: 'orders/:id/detail', element: <OrderDetailSection /> },
+                ]
+            },
+            { path: '*', element: <NotFound /> },
+        ]
+    },
+    {
+        path: "/",
+        element: <AuthLayout />,
+        children: [
+            { path: 'login', element: <Login /> },
+            { path: 'register', element: <Register /> },
+        ]
+    },
+    {
+        path: "/admin",
+        element: <AdminLayout />,
+        children: [
+            {
+                path: '',
+                element: <AdminDashboard />,
+                children: [
+                    { path: 'products', element: <AdminProductListSection /> },
+                    { path: 'orders', element: <AdminOrderListSection /> },
+                    { path: 'customers', element: <AdminCustomerListSection /> },
+                    { path: 'reports', element: <AdminSalesReportSection /> },
+                    { path: 'dashboard', element: <AdminDashboardSection /> },
+                ]
+            },
+            {
+                path: 'profile',
+                element: <AdminProfile />,
+                children: [
+                    { path: 'change-password', element: <ChangePasswordSection /> },
+                ]
+            },
+            { path: "order", element: <AdminOrderDetail /> },
+            { path: "products/create", element: <AdminCreateProduct /> },
+        ]
+    }
+])
+
+function App() {
+    return <RouterProvider router={router} />
+}
+
+export default App;
